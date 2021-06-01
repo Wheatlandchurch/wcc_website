d6083faf8d7d4c4f905a16c9d50ffef2c37193a7
wheatlandchurchadmin <81395690+wheatlandchurchadmin@users.noreply.github.com>
2021-06-01 11:59:09 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'exampledb');
+define( 'DB_NAME', getenv('DB_NAME'));
 
 /** MySQL database username */
-define( 'DB_USER', 'exampleuser');
+define( 'DB_USER', getenv('DB_USER'));
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'examplepass');
+define( 'DB_PASSWORD', getenv('DB_PASSWORD'));
 
 /** MySQL hostname */
-define( 'DB_HOST', 'db');
+define( 'DB_HOST', getenv('DB_HOST'));
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8');
