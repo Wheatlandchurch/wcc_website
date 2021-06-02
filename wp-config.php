7b28bfe4c3a91cb5a290beeaaa50d64b93083924
wheatlandchurchadmin <81395690+wheatlandchurchadmin@users.noreply.github.com>
2021-06-02 18:10:51 -0400

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,8 +1,10 @@
 <?php
-if ( ! empty( $_SERVER[‘HTTP_X_FORWARDED_HOST’] ) ) {
-$_SERVER[‘HTTP_HOST’] = $_SERVER[‘HTTP_X_FORWARDED_HOST’];
+if ( ! empty( $_SERVER['HTTP_X_FORWARDED_HOST'] ) ) {
+$_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
 }
-
+define('WP_HOME', 'https://'. filter_input(INPUT_SERVER, 'HTTP_HOST', FILTER_SANITIZE_STRING));
+define('WP_SITEURL', 'https://'. filter_input(INPUT_SERVER, 'HTTP_HOST', FILTER_SANITIZE_STRING));
+define('DOMAIN_CURRENT_SITE', filter_input(INPUT_SERVER, 'HTTP_HOST', FILTER_SANITIZE_STRING));
 /**
  * The base configuration for WordPress
  *
