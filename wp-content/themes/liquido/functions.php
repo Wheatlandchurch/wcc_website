c326015b3440506963c91f0f2f85bf126ef77015
wheatlandchurchadmin <81395690+wheatlandchurchadmin@users.noreply.github.com>
2024-02-03 12:43:45 -0500

    Add CORS to functions.php

--- wp-content/themes/liquido/functions.php
+++ wp-content/themes/liquido/functions.php
@@ -63,5 +63,25 @@ function liquido_setup() {
 } // liquido_setup()
 add_action( 'after_setup_theme', 'liquido_setup' );
 
+// CORS
+function initCors( $value ) {
+  $origin = get_http_origin();
+  $allowed_origins = [ 'wheatlandchurch.com', 'www.wheatlandchurch.com' ];
+
+  if ( $origin && in_array( $origin, $allowed_origins ) ) {
+    header( 'Access-Control-Allow-Origin: ' . esc_url_raw( $origin ) );
+    header( 'Access-Control-Allow-Methods: GET' );
+    header( 'Access-Control-Allow-Credentials: true' );
+  }
+
+  return $value;
+}
+add_action( 'rest_api_init', function() {
+
+  remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+
+  add_filter( 'rest_pre_serve_request', initCors);
+
+}, 15 );
 
 /* FIN */
